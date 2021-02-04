1854449cfed90d0026e71df7f7f432b105167130
Azamat-Sult <asultangareev@gmail.com>
2021-02-04 20:50:45 +0500

    3.3. Разбор первой программы. [#464899]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Azamat Sultangareev");
+        System.out.println("16.07.1984");
+    }
+}
