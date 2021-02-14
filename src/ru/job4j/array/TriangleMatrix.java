55577248a832baa72a74ff9e9606f9a788f7ca9b
Azamat-Sult <asultangareev@gmail.com>
2021-02-15 01:13:03 +0500

    16. Заполнить треугольную матрицу

---
+++ src/ru/job4j/array/TriangleMatrix.java
@@ -0,0 +1,17 @@
+package ru.job4j.array;
+
+public class TriangleMatrix {
+
+    public static int[][] rows(int count) {
+        int[][] triangle = new int[count][];
+        int num = 1;
+        for (int row = 0; row < count; row++) {
+            triangle[row] = new int[row + 1];
+            for (int cell = 0; cell <= row; cell++) {
+                triangle[row][cell] = num;
+                num++;
+            }
+        }
+        return triangle;
+    }
+}
