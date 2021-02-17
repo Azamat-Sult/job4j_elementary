fa27422ae8602810909235a495ff2f3936948787
Azamat-Sult <asultangareev@gmail.com>
2021-02-17 14:13:05 +0500

    47. Объединение множеств

---
+++ src/ru/job4j/array/OrArray.java
@@ -0,0 +1,40 @@
+package ru.job4j.array;
+
+public class OrArray {
+
+    @SuppressWarnings("checkstyle:SimplifyBooleanExpression")
+    public static int[] or(int[] left, int[] right) {
+        int count = 0;
+        boolean elementIn;
+        int[] temp = new int[left.length + right.length];
+        for (int i : left) {
+            elementIn = false;
+            for (int j : temp) {
+                if (i == j) {
+                    elementIn = true;
+                    break;
+                }
+            }
+            if (!elementIn) {
+                temp[count] = i;
+                count++;
+            }
+        }
+        for (int i : right) {
+            elementIn = false;
+            for (int j : temp) {
+                if (i == j) {
+                    elementIn = true;
+                    break;
+                }
+            }
+            if (!elementIn) {
+                temp[count] = i;
+                count++;
+            }
+        }
+        int[] rsl = new int[count];
+        System.arraycopy(temp, 0, rsl, 0, count);
+        return rsl;
+    }
+}
